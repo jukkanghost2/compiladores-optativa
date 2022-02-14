400e960a480daf059321ca8ae66e3a2e3c1d8b17
jukkanghost <jukkanghost@gmail.com>
2022-02-14 18:58:33 -0300

    avances codigo

---
+++ gcc/codigo-ejemplo.c
@@ -0,0 +1,17 @@
+int main() {
+
+    int a = 20;
+    int b = 30;
+    int c = 0;
+
+    c = a + b;
+    c = b - a;
+
+    for (int i = 0; i < c; i++)
+    {
+        a = a - c;
+    }
+    
+
+    return 0;
+}
